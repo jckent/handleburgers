0d8abef1273a5c407772659901d52d4fd1b95877
jckent <chasekent30@gmail.com>
2020-01-26 14:44:26 -0500

    turned cats to burgers, but not like youre thinking

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (name, devoured) VALUES ('NotaBigMac', false);
+INSERT INTO burgers (name, devoured) VALUES ('IamAGooMan', false);
+INSERT INTO burgers (name, devoured) VALUES ('JustAnotherBBQBurger', false);
+
